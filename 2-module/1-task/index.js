4068bda579dc7036cabd9333f4ff9028b5227abe
Roman Maklakov <mclakov@gmail.com>
2020-05-25 11:40:41 +0300

    Зарплаты

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,11 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-
+  let s = 0;
+  for (let key in salaries) {
+    if (isFinite(salaries[key])) {
+    s = s + salaries[key];
+    }
+  }
+    return s;
 }
