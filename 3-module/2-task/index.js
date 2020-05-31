697bc59d514252f805bda9cc43cc263d34512a61
Roman Maklakov <mclakov@gmail.com>
2020-05-31 18:24:01 +0300

    minMax

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,15 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  let arr = str
+        .split(',')
+        .join(' ')
+        .split(' ')
+        .filter(item => !isNaN(parseInt(item)));
+
+  let result = {
+  }
+  result.min = Math.min.apply(null, arr);
+  result.max = Math.max.apply(null, arr);
+  return result;
 }
