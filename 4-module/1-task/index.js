0542ddcd8285a5ebd8b3be1792322aba6a4de1a1
Roman Maklakov <mclakov@gmail.com>
2020-06-03 22:06:59 +0300

    makeFriendsList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,7 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  const newUl = document.createElement('ul');
+  newUl.innerHTML = friends.map(i => `<li>${i.firstName} ${i.lastName}</li>`).join('\n');
+  return newUl;
 }
