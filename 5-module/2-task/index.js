b979053ae2cf2ba869db7867746fc0b02613b66d
Roman Maklakov <mclakov@gmail.com>
2020-06-16 23:00:02 +0300

    SortableTable

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,29 +1,61 @@
-/**
- * Компонент, который реализует сортируемую таблицу
- * @param {Array} items - данные, которые нужно отобразить
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      },
- *
- * @constructor
- */
-function SortableTable(items) {
-  /**
-   * @property {Element} - обязательное свойство, которое ссылается на элемент <table>
-   */
-  this.el = document.createElement('table');
 
-  /**
-   * Метод выполняет сортировку таблицы
-   * @param {number} column - номер колонки, по которой
-   * нужно выполнить сортировку (отсчет начинается от 0)
-   * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
-   */
-  this.sort = (column, desc = false) => {};
+function SortableTable(items) {
+this.el = document.createElement('table');
+const thead = document.createElement('thead');
+const tbody = document.createElement('tbody');
+this.el.appendChild(thead);
+this.el.appendChild(tbody);
+thead.innerHTML = `
+<tr>
+<td>name</td>
+<td>age</td>
+<td>salary</td>
+<td>city</td>
+</tr>
+`;
+function render (){
+tbody.innerHTML =  items.map (item => {
+let row = "";
+for (key in item){
+  row += `<td>${item[key]}</td>`;
 }
+return `<tr>${row}</tr>`;
+}).join('');
+};
+render ();
+
+this.sort = (column, desc = false) => {
+items = items.sort((a, b) => {
+if (column === 0 && desc === true){
+  return a.name < b.name ? 1 : -1;
+}
+if (column === 0){
+  return a.name > b.name ? 1 : -1;
+}
+if (column === 2){
+  return a.salary > b.salary ? 1 : -1;
+}
+})
+render ();
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /  this.sort = (column, desc = false) => {};
+//
